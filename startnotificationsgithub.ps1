a4200ff4590188a0a08fd3b33b1f915aca815671
Evan Vera <74469557+devovevo@users.noreply.github.com>
2023-02-25 18:37:02 -0500

    Changed Startup Powershell - Added ability to check for wifi before starting ticket notifier - Also checks to see if chromedriver is using to much memory,   and if so, kills it

--- startnotificationsgithub.ps1
+++ startnotificationsgithub.ps1
@@ -1,2 +1,31 @@
 ./ccss_ticketing_venv/Scripts/activate
-python ccss_ticketing_notify.py
+
+function startNotifierWithWifi {
+    while (-not (Test-Connection google.com -Count 1 -Quiet)) {
+        Start-Sleep -Seconds 15
+    }
+
+    python .\ccss_ticketing_notify.py
+}
+
+for(;;) {
+    try {
+        If (!(Get-Process -Name chromedriver -ErrorAction SilentlyContinue)) {
+            startNotifierWithWifi
+        }
+        
+        $proc = Get-Process -Name chromedriver | Sort-Object -Property ProcessName -Unique -ErrorActionSilentlyContinue
+        
+        If (!$proc -or ($proc.Responding -eq $false) –or ($proc.WorkingSet -GT 50000*1024)) 
+        {
+            $proc.Kill()
+            Start-Sleep -s 10
+            startNotifierWithWifi
+        }
+    }
+    catch {
+        Write-Output "Error starting or closing notifier"
+    }
+    
+    Start-sleep -s 60
+}
